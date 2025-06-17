f444dc13b3f785185c4598502658fb91388122ee
jesusdangerous <aleksandrzonov641@gmail.com>
2025-06-17 20:29:28 +0500

    Update front for registration

--- front/vite.config.js
+++ front/vite.config.js
@@ -1,16 +1,16 @@
 import { fileURLToPath, URL } from 'node:url'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
-
 import { defineConfig } from 'vite';
 
 export default defineConfig({
   server: {
-    port: parseInt(process.env.PORT) || 3000,
+    port: 3000,
     host: true,
   },
   preview: {
-    port: parseInt(process.env.PORT) || 3000,
+    port: isNaN(parseInt(process.env.PORT)) ? 3000 : parseInt(process.env.PORT),
+    host: true,
     allowedHosts: ['cosmetic-telegram-mini-app.onrender.com']
   },
   resolve: {
@@ -18,9 +18,5 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  plugins: [vue()], // Добавьте плагин здесь
-  server: {
-    port: 3000,
-    host: true
-  }
+  plugins: [vue()],
 });
