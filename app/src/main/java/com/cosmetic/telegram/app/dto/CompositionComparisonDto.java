46f35f382f87bc4cad07dd015c0201d31abf1d5e
jesusdangerous <aleksandrzonov641@gmail.com>
2025-06-19 19:30:37 +0500

    Add composition comparison dto

---
+++ app/src/main/java/com/cosmetic/telegram/app/dto/CompositionComparisonDto.java
@@ -0,0 +1,9 @@
+package com.cosmetic.telegram.app.dto;
+
+import lombok.Data;
+
+@Data
+public class CompositionComparisonDto {
+    private String firstComposition;
+    private String secondComposition;
+}
