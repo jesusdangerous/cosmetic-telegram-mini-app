074058e463a9965329becf74e64d961befae7e78
zonov <140524793+jesusdangerous@users.noreply.github.com>
2025-06-20 01:10:11 +0500

    Update SecurityConfig.java

--- app/src/main/java/com/cosmetic/telegram/app/config/SecurityConfig.java
+++ app/src/main/java/com/cosmetic/telegram/app/config/SecurityConfig.java
@@ -21,11 +21,8 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/**").permitAll()
-                        .anyRequest().authenticated()
-                );
+            .csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(auth -> auth.anyRequest().permitAll());  
         return http.build();
     }
 
